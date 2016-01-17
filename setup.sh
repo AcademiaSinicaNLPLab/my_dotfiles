edfdef8aa7d8710542e69901beb8b0ff3f7c76ec
ipod825 <ipod825@gmail.com>
2016-01-17 18:03:22 +0800

    check file exits

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ echo "Setting up the dotfiles..."
 find dotfiles -name ".*" | cut -d / -f 2 | xargs -i ln -sfv ${PWD##*/}/dotfiles/{} ../{}
 find dotfiles -name ".*" | cut -d / -f 2 | xargs -i ln -sfv dotfiles/{} {}
 echo "Setting neovim"
-curl -fLo ~/.nvim/autoload/plug.vim --create-dirs  https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-ln -s ~/.nvim ~/.config/nvim
-ln -s ~/.nvimrc ~/.config/nvim/init.vim
+[ ! -f ~/.nvim/autoload/plug.vim ] && curl -fLo ~/.nvim/autoload/plug.vim --create-dirs  https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+[ ! -h ~/.config/nvim ] && ln -s ~/.nvim ~/.config/nvim
+[ ! -h ~/.config/nvim/init.vim ] && ln -s ~/.nvimrc ~/.config/nvim/init.vim
 echo "Done"
